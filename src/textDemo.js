4f1ba9480303945d19fa4f66dca6c3ff87eccf84
Monday <axu1997@qq.com>
2021-03-24 16:25:06 +0800

    webpack处理js

---
+++ src/textDemo.js
@@ -0,0 +1,14 @@
+// function *gen(x){
+//   var index = 1
+//   var ccc;
+//   // while(true){
+//   //   yield index++
+//   // }
+//   ccc = (yield index++)
+//   index--
+// }
+// let it = gen(10)
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+console.log(process.env.NODE_ENV);
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+  entry:'./src/textDemo.js',
+  mode:'development',
+  output:{
+    path:path.resolve(__dirname, 'dist'),
+    filename:'bundle.js'
+  }
+}
